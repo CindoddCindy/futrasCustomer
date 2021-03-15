f270a0a85b074c7dd65d0ac7616981b4d918619d
CindoddCindy <cindodcindy@gmail.com>
2021-03-15 16:31:17 +0700

    tambah activity dan fragment

---
+++ app/src/main/java/com/tess/futrash/view/Regis.java
@@ -0,0 +1,16 @@
+package com.tess.futrash.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tess.futrash.R;
+
+public class Regis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regis);
+    }
+}
