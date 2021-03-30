2294cdf9eb10b8d9f4882c2a0e7ec80910b2e52c
CindoddCindy <cindodcindy@gmail.com>
2021-03-30 19:51:26 +0700

    tambah detail confirm order detail

---
+++ app/src/main/java/com/tess/futrash/view/ConfirmOrderDetailActivity.java
@@ -0,0 +1,16 @@
+package com.tess.futrash.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tess.futrash.R;
+
+public class ConfirmOrderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_order_detail);
+    }
+}
