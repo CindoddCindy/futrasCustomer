1e640fc3f535858f2ded833abda0adda3b7b94b8
CindoddCindy <cindodcindy@gmail.com>
2021-04-08 20:37:17 +0700

    add btn order

--- app/src/main/java/com/tess/futrash/view/ItemDetail.java
+++ app/src/main/java/com/tess/futrash/view/ItemDetail.java
@@ -180,6 +180,25 @@ public class ItemDetail extends AppCompatActivity {
     //send pake bundle
     public void addToOrder(){
 
+        Bundle bundle = new Bundle();
+        //bundle.putLong("id_user",content.getUser().getId());
+        bundle.putString("jm", textView_nama_makanan.getText().toString());
+        bundle.putString("tds",textView_tidak_dikonsumsi_sejak.getText().toString());
+        bundle.putString("dk",textView_dijual_karena.getText().toString());
+        bundle.putString("bm",textView_berat_makanan.getText().toString());
+        bundle.putString("nt",textView_nama_toko.getText().toString());
+        bundle.putString("np",textView_nama_penjual.getText().toString());
+        bundle.putString("lm",textView_lokasi_makanan.getText().toString());
+        bundle.putString("hm",textView_harga_makanan.getText().toString());
+        bundle.putString("sp",textView_saran_penggunaan.getText().toString());
+        bundle.putString("kk",textView_kandungan_kimia.getText().toString());
+        bundle.putString("pn",textView_phone_num.getText().toString());
+        bundle.putString("ca",textView_date_item.getText().toString());
+
+        Intent intent = new Intent(ItemDetail.this, OrderPage.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
     }
 
 }
