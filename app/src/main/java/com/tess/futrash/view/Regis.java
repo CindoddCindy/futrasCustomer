1e66b5d30eb5392062d1fd5257858d9096dd42b8
CindoddCindy <cindodcindy@gmail.com>
2021-04-08 10:31:22 +0700

    add regis respon

--- app/src/main/java/com/tess/futrash/view/Regis.java
+++ app/src/main/java/com/tess/futrash/view/Regis.java
@@ -2,16 +2,29 @@ package com.tess.futrash.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.tess.futrash.R;
+import com.tess.futrash.model.pojo_regis.RegisCustomerRespon;
+import com.tess.futrash.servis.MethodsFactory;
+import com.tess.futrash.servis.RetrofitHandle;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Regis extends AppCompatActivity {
     private EditText editText_name, editText_email, editText_phone, editText_password;
     private TextView textView_btn_regis_submit;
+    private ImageView imageView_have_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,15 @@ public class Regis extends AppCompatActivity {
         editText_email=findViewById(R.id.et_regis_email);
         editText_phone=findViewById(R.id.et_regis_phone);
         editText_password=findViewById(R.id.et_regis_pass);
+        imageView_have_account=findViewById(R.id.iv_regis_have_account);
+
+        imageView_have_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Regis.this,Login.class);
+                startActivity(intent);
+            }
+        });
 
         textView_btn_regis_submit=findViewById(R.id.tv_btn_regis);
 
@@ -47,5 +69,78 @@ public class Regis extends AppCompatActivity {
 
     public void regisCustomer(){
 
-    }
+        String name = editText_name.getText().toString();
+        String email = editText_email.getText().toString();
+        String phone = editText_phone.getText().toString();
+        String password = editText_password.getText().toString();
+
+        JsonObject jsonObject = new JsonObject();
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add("buyer");
+        jsonArray.add("user");
+
+        jsonObject.addProperty("name", name);
+        jsonObject.addProperty("email",email );
+        jsonObject.addProperty("phone", phone);
+        jsonObject.addProperty("password", password);
+        jsonObject.add("role", jsonArray);
+
+
+
+        MethodsFactory methodsFactory =  RetrofitHandle.getRetrofitLink().create(MethodsFactory.class);
+        Call<RegisCustomerRespon> call= methodsFactory.isRegistration(jsonObject);
+        call.enqueue(new Callback<RegisCustomerRespon>() {
+            @Override
+            public void onResponse(Call<RegisCustomerRespon> call, Response<RegisCustomerRespon> response) {
+                if(response.isSuccessful()){
+                    RegisCustomerRespon regisCustomerRespon=response.body();
+                    regisCustomerRespon.getMessage();
+                    Intent intent = new Intent(Regis.this,Login.class);
+                    startActivity(intent);
+                 /*   spHandle.saveSPBoolean(SpHandle.SP_HAVE_LOGIN, true);
+                    startActivity(new Intent(Regis.this, Login.class)
+                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                    finish();
+
+                  */
+
+
+
+                }
+
+                else {
+                    // error case
+                    switch (response.code()) {
+                        case 404:
+                            Toast.makeText(Regis.this, " not found", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 500:
+                            Toast.makeText(Regis.this, "server error", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 401:
+                            Toast.makeText(Regis.this, " sorry can't authenticated, try again", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        default:
+                            Toast.makeText(Regis.this, "unknown error ", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<RegisCustomerRespon> call, Throwable t) {
+                Toast.makeText(Regis.this, "network failure :( inform the user and possibly retry ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
+
+
+}
 }
