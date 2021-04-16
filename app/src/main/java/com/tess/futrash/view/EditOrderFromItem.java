8117821356983693aec462c39220f96a93ba55c4
CindoddCindy <cindodcindy@gmail.com>
2021-04-16 15:38:28 +0700

    tambah layout edit order

---
+++ app/src/main/java/com/tess/futrash/view/EditOrderFromItem.java
@@ -0,0 +1,16 @@
+package com.tess.futrash.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tess.futrash.R;
+
+public class EditOrderFromItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_order_from_item);
+    }
+}
